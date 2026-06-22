e417f57c75f2eab92a1dacf4b511404a17c7297f
Divyaa Makhija <2024.divyaa.makhija@ves.ac.in>
2026-06-22 18:47:28 +0530

    Added roadmap generator logic

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -11,10 +11,100 @@ function startApp() {
 }
 function generateRoadmap() {
 
-    let role = document.getElementById("role").value;
+    const role = document.getElementById("role").value;
+    const result = document.getElementById("result");
 
-    window.location.href =
-        "/roadmap?role=" + role;
+    let roadmap = "";
+
+    if(role === "AI Engineer") {
+        roadmap = `
+        <h3>AI Engineer Roadmap</h3>
+
+        <p><b>Beginner</b></p>
+        <ul>
+            <li>Python</li>
+            <li>Git & GitHub</li>
+            <li>SQL</li>
+        </ul>
+
+        <p><b>Intermediate</b></p>
+        <ul>
+            <li>Machine Learning</li>
+            <li>Pandas</li>
+            <li>TensorFlow</li>
+        </ul>
+
+        <p><b>Advanced</b></p>
+        <ul>
+            <li>Deep Learning</li>
+            <li>LLMs</li>
+            <li>MLOps</li>
+        </ul>
+        `;
+    }
+
+    else if(role === "Frontend Developer") {
+        roadmap = `
+        <h3>Frontend Developer Roadmap</h3>
+
+        <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>React</li>
+            <li>Next.js</li>
+            <li>Deployment</li>
+        </ul>
+        `;
+    }
+
+    else if(role === "Backend Developer") {
+        roadmap = `
+        <h3>Backend Developer Roadmap</h3>
+
+        <ul>
+            <li>Java</li>
+            <li>Spring Boot</li>
+            <li>SQL</li>
+            <li>REST APIs</li>
+            <li>Docker</li>
+            <li>AWS</li>
+        </ul>
+        `;
+    }
+
+    else if(role === "Cybersecurity Analyst") {
+        roadmap = `
+        <h3>Cybersecurity Roadmap</h3>
+
+        <ul>
+            <li>Networking</li>
+            <li>Linux</li>
+            <li>Ethical Hacking</li>
+            <li>Burp Suite</li>
+            <li>OWASP</li>
+            <li>Penetration Testing</li>
+        </ul>
+        `;
+    }
+
+    else {
+        roadmap = `
+        <h3>Data Scientist Roadmap</h3>
+
+        <ul>
+            <li>Python</li>
+            <li>Statistics</li>
+            <li>Pandas</li>
+            <li>Machine Learning</li>
+            <li>Data Visualization</li>
+            <li>Deep Learning</li>
+        </ul>
+        `;
+    }
+
+    result.innerHTML = roadmap;
+}
 }
 function analyzeSkills() {
 
